b6eb3b93f7324cf61e33f6eeafa40ccfd18c6a2a
Jaco Bovenschen <jbovenschen@jouwomgeving.nl>
2020-04-05 14:23:35 +0200

    Trim css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,27 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./pages/**/*", "./components/**/*", "./templates/**/*"],
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
-  plugins: ["postcss-import", "tailwindcss", "autoprefixer"],
+  plugins: [
+    "postcss-import",
+    "tailwindcss",
+    "autoprefixer",
+    process.env.NODE_ENV === "production"
+      ? [
+          "@fullhuman/postcss-purgecss",
+          {
+            content: [
+              "./pages/**/*.js",
+              "./components/**/*.js",
+              "./templates/**/*.js",
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        ]
+      : undefined,
+    "postcss-preset-env",
+  ],
 };
