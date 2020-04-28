c5e84644479075c58c436cdf58aef27aea438a34
Jaco Bovenschen <jbovenschen@jouwomgeving.nl>
2020-04-28 21:56:22 +0200

    Simplify GA

--- components/googleAnalytics.js
+++ components/googleAnalytics.js
@@ -1,12 +1,18 @@
 // googleAnalytics.js
-
+import React from "react";
 import ReactGA from "react-ga";
 
-export const initGA = () => {
-  ReactGA.initialize("UA-164861015-1");
+const init = () => {
+  if (process.browser) {
+    ReactGA.initialize("UA-164861015-1");
+  }
 };
 
-export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
-};
+function useLogPageVisit() {
+  React.useEffect(() => {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  }, []);
+}
+
+export { init, useLogPageVisit };
--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,13 @@ import React from "react";
 import App from "next/app";
 import "../css/app.css";
 
+import { init } from "../components/googleAnalytics";
+
 import { MDXProvider } from "@mdx-js/react";
 
 import * as components from "../components/MDX";
+
+init();
 
 class MyApp extends App {
   render() {
